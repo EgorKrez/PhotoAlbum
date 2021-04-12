277cc56b9844798a0e227a764e496904e186886d
Yahor Krez <Yahor.Krez@itechart-group.com>
2021-04-12 19:04:01 +0300

    Application has been updated

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Photo(props) {
+
+    useEffect(() => {
+        console.log(props.photo);
+    }, []) 
+
     return (        
         <div>
         {JSON.stringify(props.photo)}    
